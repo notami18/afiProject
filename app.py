b326affa7f043c9a33d33ac26302399081b933a4
Andres Minota <andresnotami@gmail.com>
2024-02-28 23:30:41 -0500

    Agrega configuración de Flask y plantilla Jinja2

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from openpyxl import load_workbook
 from flask import Flask , render_template , request
 
 # Carga del archivo Excel
-wb = load_workbook ( 'pronunciacion.xlsx' )
+wb = load_workbook ('data.xlsx')
 sheet = wb.active
 
 # Creación del diccionario
 diccionario = {}
-for row in sheet.iter_rows ( min_row=2 ):
-    palabra = row [ 0 ].value
-    pronunciacion = row [ 1 ].value
-    diccionario [ palabra ] = pronunciacion
+for row in sheet.iter_rows (min_row=2):
+    palabra = row [0].value.strip().lower()
+    pronunciacion = row [1].value.strip().lower()
+    diccionario [palabra] = pronunciacion
 
 # Aplicación Flask
 app = Flask ( __name__ )
@@ -23,16 +23,16 @@ def index():
 
 @app.route ( '/traducir' , methods=[ 'POST' ] )
 def traducir():
-    texto = request.form [ 'texto' ]
+    texto = request.form ['texto']
     traduccion = ''
 
     # Separación por palabras
-    palabras = texto.split ()
+    palabras = texto.split()
 
     # Traducción y concatenación
     for palabra in palabras:
         if palabra in diccionario:
-            traduccion += diccionario [ palabra ]
+            traduccion += diccionario[palabra]
         else:
             traduccion += palabra
         traduccion += ' '
