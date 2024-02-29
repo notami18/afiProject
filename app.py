305a7a6d0a933879693839264c4bf00e39430d65
Andres Minota <andresnotami@gmail.com>
2024-02-28 22:46:48 -0500

    Agregados archivos de configuración de IDEA y código de la aplicación Flask

---
+++ app.py
@@ -0,0 +1,44 @@
+from openpyxl import load_workbook
+from flask import Flask , render_template , request
+
+# Carga del archivo Excel
+wb = load_workbook ( 'pronunciacion.xlsx' )
+sheet = wb.active
+
+# Creación del diccionario
+diccionario = {}
+for row in sheet.iter_rows ( min_row=2 ):
+    palabra = row [ 0 ].value
+    pronunciacion = row [ 1 ].value
+    diccionario [ palabra ] = pronunciacion
+
+# Aplicación Flask
+app = Flask ( __name__ )
+
+
+@app.route ( '/' )
+def index():
+    return render_template ( 'index.html' )
+
+
+@app.route ( '/traducir' , methods=[ 'POST' ] )
+def traducir():
+    texto = request.form [ 'texto' ]
+    traduccion = ''
+
+    # Separación por palabras
+    palabras = texto.split ()
+
+    # Traducción y concatenación
+    for palabra in palabras:
+        if palabra in diccionario:
+            traduccion += diccionario [ palabra ]
+        else:
+            traduccion += palabra
+        traduccion += ' '
+
+    return render_template ( 'index.html' , traduccion=traduccion )
+
+
+if __name__ == '__main__':
+    app.run ( debug=True )
